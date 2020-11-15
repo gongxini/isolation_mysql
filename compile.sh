316204c61a6b293e6d5a14950bc0ab28e36a9fcc
gongxini <yigongh@gmail.com>
2020-11-15 19:01:17 +0800

    Add 5.6.22

---
+++ compile.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p {build,dist}
+cd build
+
+cmake ../5.6.22  -DCMAKE_INSTALL_PREFIX=`pwd`/../dist \
+  -DMYSQL_DATADIR=`pwd`/../dist/data -DWITH_DEBUG=1 -DCMAKE_BUILD_TYPE=DEBUG \
+  -DCMAKE_C_FLAGS_DEBUG="-g -O0" -DCMAKE_CXX_FLAGS_DEBUG="-g -O0" \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DMYSQL_MAINTAINER_MODE=false
+
+make -j4
+if [ $? -ne 0 ]; then
+  echo "Failed to build mysql"
+  exit 1
+fi
+make install
