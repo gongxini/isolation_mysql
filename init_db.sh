4cf91a4040b3fae2d400663e226eafd93f1c37b3
gongxini <yigongh@gmail.com>
2021-04-06 22:04:22 +0800

    Add configure and init

---
+++ init_db.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+if [ ! -d dist ]; then
+  echo "dist directory does not exist. Must be called after compile.sh succeeds."
+  exit 1
+fi
+
+cd dist
+
+if [ ! -x bin/mysqld -o ! -x scripts/mysql_install_db ]; then
+  echo "bin/mysqld or scripts/mysql_install_db does not exist"
+  exit 1
+fi
+
+basedir=`pwd`
+
+echo "Copying and customizing support-files/my-small.cnf to my.cnf..."
+
+sed -e "s#socket\s*=\s*/tmp/mysql.sock#socket\t=\t$basedir/mysqld.sock#g" \
+  -e "/^# Here follows entries/a [mysqld_safe]\nsocket\t=\t$basedir/mysqld.sock\nnice\t=\t0" \
+  -e "/^skip-external-locking/i basedir\t=\t$basedir\ndatadir\t=\t$basedir/data" \
+  support-files/my-small.cnf > my.cnf
+
+echo "Installing db..."
+scripts/mysql_install_db  --basedir=$basedir --datadir=$basedir/data
+
+echo "Creating initial tables and data..."
+./bin/mysqld --defaults-file=my.cnf  &
+sleep 10
+./bin/mysql -S mysqld.sock << EOF
+use test;
+CREATE TABLE tbl(id INT NOT NULL AUTO_INCREMENT,col INT NOT NULL, PRIMARY KEY (id)) Engine = InnoDB;
+INSERT INTO tbl(col) VALUES(11);
+INSERT INTO tbl(col) VALUES(12);
+INSERT INTO tbl(col) VALUES(13);
+INSERT INTO tbl(col) VALUES(14);
+INSERT INTO tbl(col) VALUES(15);
+INSERT INTO tbl(col) VALUES(16);
+INSERT INTO tbl(col) VALUES(17);
+INSERT INTO tbl(col) VALUES(18);
+INSERT INTO tbl(col) VALUES(19);
+INSERT INTO tbl(col) VALUES(20);
+INSERT INTO tbl(col) VALUES(21);
+INSERT INTO tbl(col) VALUES(22);
+INSERT INTO tbl(col) VALUES(23);
+INSERT INTO tbl(col) VALUES(24);
+INSERT INTO tbl(col) VALUES(25);
+INSERT INTO tbl(col) VALUES(26);
+INSERT INTO tbl(col) VALUES(27);
+INSERT INTO tbl(col) VALUES(28);
+INSERT INTO tbl(col) VALUES(29);
+INSERT INTO tbl(col) VALUES(30);
+CREATE TABLE tbl1(id INT NOT NULL AUTO_INCREMENT,col INT NOT NULL, PRIMARY KEY (id)) Engine = MyISAM;
+INSERT INTO tbl1(col) VALUES(31);
+INSERT INTO tbl1(col) VALUES(32);
+INSERT INTO tbl1(col) VALUES(33);
+INSERT INTO tbl1(col) VALUES(34);
+EOF
+./bin/mysqladmin -S mysqld.sock -u root shutdown
+
+echo "Successfully initialized mysql db"
